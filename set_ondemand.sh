223032ae08007e041f4799acfd3641b359f4c14f
rock64 <rock64@rockpro64>
2018-11-13 19:03:36 +0000

    temp script, governor scripts

---
+++ set_ondemand.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo ondemand | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
---
+++ temp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cat /sys/class/thermal/thermal_zone0/temp
+cat /sys/class/thermal/thermal_zone1/temp
+#cpufreq-info
